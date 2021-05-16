7c73331af5d0d521fbb5a63a25bea9d701eced53
Shubham <shubham@Admins-MacBook-Air.local>
2021-05-17 03:02:29 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React, {useState} from "react";
+import dustbinicon from './dustbin.png'
+
+
 
 function App() {
+  const[taskInput, updateTaskInput]=useState("");
+  const [toDoList,updateToDoList]=useState([]);
+  
+  const addNote=()=>{
+    toDoList.push({description: taskInput})
+    updateToDoList(toDoList)
+    updateTaskInput("")
+
+  }
+
+  const deleteTask= (index)=>{
+  const newList = toDoList.filter((item,i)=> i!== index);
+  updateToDoList(newList)
+
+
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-background">
+      <p className="heading-text">To-Do-List</p>
+      <div className="task-container">
+        <div>
+        <input className="text-input" value={taskInput} 
+        onChange={(event)=>updateTaskInput(event.target.value)} />
+        <button className="add-button" onClick={addNote}>ADD</button>
+        </div>
+
+        {toDoList?.length?toDoList.map((toDoObject,index)=>
+        
+        <ListItem index={index} itemData={toDoObject} deleteTask={deleteTask}/>):
+        
+        <p className="no-item-text">No Task Added!</p>} 
+        
+      </div>
+      <p className="footer-text">Shubham</p>
     </div>
+    
   );
 }
+
+function ListItem(props){
+  return (
+  <div classname="list-item row jc-space-between">
+    <span>{props.itemData.description }</span>
+    {/* returning different input values */}
+    <img src={dustbinicon} className="delete-icon" onClick={()=>props.deleteTask(props.index)}/>
+
+  </div>
+    );
+}
+
+
 
 export default App;
