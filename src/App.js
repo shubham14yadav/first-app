0d0f24c4db042956a6a99a88591027c390ee7af6
Shubham14yadav <shubham14yadav@gmail.com>
2021-05-18 00:53:56 +0530

    commit on "https://github.com/shubham14yadav/new.git"

--- src/App.js
+++ src/App.js
@@ -14,28 +14,39 @@ function App() {
     updateTaskInput("")
 
   }
+  // keycode 13 is for using 'enter' button
+  const inputKeyDown=(event)=>{
+    if (event.keyCode===13)
+        addNote();
+
+  }
 
   const deleteTask= (index)=>{
   const newList = toDoList.filter((item,i)=> i!== index);
   updateToDoList(newList)
-
-
-
   }
+
+  const markComplete = (index)=>{
+    const list=[...toDoList];
+    // copied toDoList into list
+    list[index].isComplete = !list[index].isComplete;
+    updateToDoList(list)
+  }
+
 
   return (
     <div className="app-background">
       <p className="heading-text">To-Do-List</p>
       <div className="task-container">
         <div>
-        <input className="text-input" value={taskInput} 
+        <input className="text-input" value={taskInput} onKeyDown={inputKeyDown}   //onKeyDown is for enter
         onChange={(event)=>updateTaskInput(event.target.value)} />
         <button className="add-button" onClick={addNote}>ADD</button>
         </div>
 
         {toDoList?.length?toDoList.map((toDoObject,index)=>
         
-        <ListItem index={index} itemData={toDoObject} deleteTask={deleteTask}/>):
+        <ListItem index={index} itemData={toDoObject} deleteTask={deleteTask} markComplete={markComplete} />):
         
         <p className="no-item-text">No Task Added!</p>} 
         
@@ -49,7 +60,9 @@ function App() {
 function ListItem(props){
   return (
   <div classname="list-item row jc-space-between">
-    <span>{props.itemData.description }</span>
+    <span className={props.itemData.isComplete?"task-complete":""}
+    onClick={()=>props.markComplete(props.index)}> 
+    {props.itemData.description }</span>
     {/* returning different input values */}
     <img src={dustbinicon} className="delete-icon" onClick={()=>props.deleteTask(props.index)}/>
 
